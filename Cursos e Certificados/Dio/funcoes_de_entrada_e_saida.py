cd3df064efaedaaf3fc24dffe503cbbb3468ba2c
Hesterzinha <yasmin.silva8@estudante.ifms.edu.br>
2025-04-03 09:34:19 -0400

    Curso DIO

---
+++ Cursos e Certificados/Dio/funcoes_de_entrada_e_saida.py
@@ -0,0 +1,7 @@
+nome = input('digite seu nome')
+sobrenome = input(' DIGITE SEU SOBRENOME')
+
+print(nome, sobrenome)
+print(nome, sobrenome, end=".../n")
+print(nome, sobrenome, sep="a", end=".../n")
+print(nome, sobrenome, sep="a")
--- Cursos e Certificados/Dio/variaveis_e_constantes.py
+++ Cursos e Certificados/Dio/variaveis_e_constantes.py
@@ -1,1 +1,12 @@
+nome = 'guilerme'
+idade = 28
 
+nome = 'giovvana'
+
+print(nome, idade)
+
+limite_saque_diario = 1000  # snake case
+
+CONSTANTES_STATUS = ['SP', 'RJ', 'SC', 'RS']
+
+print(CONSTANTES_STATUS)
