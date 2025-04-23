59d57b817d27094209bad8637a1cf294641d3bbd
Hesterzinha <yasmin.silva8@estudante.ifms.edu.br>
2025-04-23 14:29:20 -0400

    Algoritmo --> Aula e Exercicios

---
+++ Algoritmo_1/Aula3.py
@@ -0,0 +1,19 @@
+'''
+Funcionamento do for no código:
+
+Iteração: O for percorre cada número na sequência gerada por range(1, n+1).
+
+Condição: Para cada número i, verifica se n % i == 0 (ou seja, se i é divisor de n).
+
+Ação: Se a condição for verdadeira, incrementa o contador divisores em 1.
+
+Finalização: Após o laço terminar, o programa sabe quantos divisores o número n possui.
+
+Resumo geral do for:
+O laço for é usado para repetir um bloco de código para cada elemento de uma sequência.
+
+No caso do código, ele percorre os números de 1 até n e realiza uma verificação para cada número.
+
+Resultado:
+Ao final do laço, o programa imprime a quantidade total de divisores de n.
+'''
---
+++ Algoritmo_1/Exercicios_10_04_2025/MEDIO/ex3.py
@@ -0,0 +1,3 @@
+n = int(input('digite um numero: '))
+soma = 0
+for i in range(1, int(n)+1):
